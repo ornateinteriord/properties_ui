195116d61d866cd4b16c017da335c3597858958a
spexzee <abub5979@gmail.com>
2025-03-08 13:56:19 +0530

    Added Property-types api

---
+++ src/api/Property-Types/index.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
+import { get } from "../Api"
+
+export const useGetPropertyTypes = () => {
+    return useQuery({
+        queryKey : ["property-types"],
+        queryFn : async () => {
+            const response = await get('/property-type/getall')
+            if (response.success) {
+                return response.data;
+              } else {
+                throw new Error(response.message || "Failed to fetch member details");
+              }
+        }
+    })
+  }
