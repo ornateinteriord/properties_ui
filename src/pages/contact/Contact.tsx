3c1b52fd0070f5ecce9c302d3a45fae3c4d15fd1
sid-1974 <sidgithub1974@gmail.com>
2025-03-11 00:29:44 +0530

    small change

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -110,7 +110,6 @@ const Contact = () => {
     <Box sx={{width:"100%",height:"100vh",marginTop:"60px",display:"flex",justifyContent:"center",alignItems:"center"}}>
     <h1>coming soon ...</h1>
    </Box>
-  )
-}
+  
 
 export default Contact;
